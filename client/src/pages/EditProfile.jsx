196dc4fbd3880094c9aeee324cf52dcc70f66d90
Samuel Littlefair-Wallace <sam@littlefairwallace.ca>
2020-05-12 14:51:18 +0200

    Start profile edit

---
+++ client/src/pages/EditProfile.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+
+export default class EditProfile extends Component {
+  render() {
+    return (
+      <div>
+        <form>
+          <label htmlFor="firstName">
+            First Name
+            <input type="text" name="" id="" />
+          </label>
+        </form>
+      </div>
+    );
+  }
+}
